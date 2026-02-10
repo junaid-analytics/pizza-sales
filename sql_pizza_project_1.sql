9738c964b9a47e7da2389a17ebe3164a9756c0d2
Shaik Junaid <shaikjunaidmubeen@gmail.com>
2026-02-10 20:50:13 +0530

    Update sql_pizza_project_1.sql

--- sql_pizza_project_1.sql
+++ sql_pizza_project_1.sql
@@ -1,3 +1,5 @@
+-- My first beginner SQL project analyzing pizza sales data to find total revenue, popular pizzas, and sales trends. Built for practice and skill improvement in Data Analytics.
+
 CREATE DATABASE pizzahut;
 use pizzahut;
 
